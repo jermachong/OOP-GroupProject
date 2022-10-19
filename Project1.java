bf7ff48a681eb2425a735a85a82012ad69c4bcd9
jaim1t6 <116021538+jaim1t6@users.noreply.github.com>
2022-10-19 16:33:51 -0400

    created menu skeleton

--- Project1.java
+++ Project1.java
@@ -3,9 +3,44 @@
  * Jeremy Achong, Jaime Chea, and Grace Maroon
  */
 
+import java.util.Scanner;
+
  public class Project1 {
     public static void main(String[] args){
+        Scanner myScan = new Scanner(System.in);
 
+        System.out.println("Choose one of the options:");
+        System.out.println("1 - Enter the information a faculty");
+        System.out.println("2 - Enter the information of a student");
+        System.out.println("3 - Print tuition invoice for a student");
+        System.out.println("4 - Print faculty information");
+        System.out.println("5 - Enter the information of a staff member");
+        System.out.println("6 - Print the information of a staff member");
+        System.out.println("7 - Exit Program");
+        System.out.println("        \nEnter your selection: ");
+        int selection = myScan.nextInt();
+
+        if(selection == 1){
+            System.out.println("");
+        }
+        else if(selection == 2){
+
+        }
+        else if(selection == 3){
+
+        }
+        else if(selection == 4){
+
+        }
+        else if(selection == 5){
+
+        }
+        else if(selection == 6){
+
+        }
+        else if(selection == 7){
+
+        }
     }
  }
 
@@ -46,6 +81,10 @@ class Faculty extends Employee{
 class Staff extends Employee{
     Super(fullName, id); 
 }
+<<<<<<< HEAD
+
+class Personnel extends Person{
+=======
 
 //Creates array of Person
 class Personnel{
@@ -58,6 +97,7 @@ class Personnel{
     public Person[] getList(){
         return list; 
     }
+>>>>>>> c101fcfd7ce46b82b5cfa307de3c2b7b544eb167
     
 }
 
