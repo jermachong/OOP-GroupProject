8985cc3dca1f736508f22813c8b019c69f7fd8ef
jaim1t6 <116021538+jaim1t6@users.noreply.github.com>
2022-10-19 17:39:43 -0400

    selection 2 - student - code to get input from user

--- Project1.java
+++ Project1.java
@@ -26,11 +26,11 @@ import java.util.Scanner;
             System.out.println("    Name of the faculty member: ");
             String newName = myScan.nextLine();
             // Get ID
-            System.out.println("ID: ");
+            System.out.println("    ID: ");
             String newID = myScan.nextLine();
             // Get Rank
             do{
-                System.out.println("Rank: ")
+                System.out.println("    Rank: ")
                 String newRank = myScan.nextLine();
                 newRank = newRank.toLowerCase(); // make the string all lowercase to compare to accepted ranks
 
@@ -38,10 +38,38 @@ import java.util.Scanner;
                     System.out.println(newRank + " is invalid");
             } while(newRank.compareTo("professor") == 1 || newRank.compareTo("adjunct") == 1)
 
-             
+            do{
+                System.out.println("    Department: ")
+                String newDepartment = myScan.nextLine();
+                newRank = newDepartment.toLowerCase(); // make the string all lowercase to compare to accepted ranks
+
+                if(newDepartment.compareTo("mathematics") == 1 || newDepartment.compareTo("engineering") == 1 || newDepartment.compareTo("sciences") == 1)
+                    System.out.println(newDepartment + " is invalid");
+            } while(newDepartment.compareTo("mathematics") == 1 || newDepartment.compareTo("engineering") == 1 || newDepartment.compareTo("sciences") == 1)
+            // create new Faculty object and add this info to it/add to Person array
+            // create new Faculty 'f' with constructor?
+            // Faculty f = new Faculty() 
+
+            System.out.println("Faculty added!");
+            }
         }
         else if(selection == 2){
+            System.out.println("Enter the student info:");
+            // Get Name
+            System.out.println("    Name of the student: ");
+            String newName = myScan.nextLine();
+            // Get ID
+            System.out.println("    ID: ");
+            String newID = myScan.nextLine();
+            // Get GPA
+            System.out.println("    GPA: ")
+            double newGPA = myScan.nextDouble();
+            // Get Credit Hours
+            System.out.println("    Credit Hours: ");
+            int newHours = myScan.nextInt();
 
+            // Create Student 's', add info. to 's', add to Person[], 
+            System.println.out("Student added!")
         }
         else if(selection == 3){
 
@@ -61,9 +89,13 @@ import java.util.Scanner;
     }
  }
 
+<<<<<<< HEAD
+class Person{
+=======
 abstract class Person{
     String fullName; 
     int id;
+>>>>>>> f7bf719b0eb626c63076579bdce4f347c5d845c0
     private String fullName; 
     private String id;
 
@@ -100,8 +132,16 @@ abstract class Person{
 
     //Other functions
 
+<<<<<<< HEAD
+    //Print out full name and ID
+    public void printPersonInfo(){
+        System.out.println("---------------------------------------------------------------------------\n\n");
+        System.out.println(fullName + "\t\t" + id); 
+    }
+=======
     //Abstract function to be overriden in Student, Fac, and Staff
     public abstract void print();
+>>>>>>> f7bf719b0eb626c63076579bdce4f347c5d845c0
 }
 
 class Student extends Person{ 
@@ -138,17 +178,32 @@ class Student extends Person{
         printPersonInfo(); //Prints seperating line + Student's info. 
     }
 }
+<<<<<<< HEAD
+class Employee extends Person{
+    Super(fullName, id); 
+    private String department; 
+=======
 abstract class Employee extends Person{
     private String department;
 
+>>>>>>> f7bf719b0eb626c63076579bdce4f347c5d845c0
 }
 
 class Faculty extends Employee{
+<<<<<<< HEAD
+    Super(fullName, id); 
+
+=======
     private String rank;
+>>>>>>> f7bf719b0eb626c63076579bdce4f347c5d845c0
 }
 
 class Staff extends Employee{
+<<<<<<< HEAD
+    Super(fullName, id); 
+=======
     private String status; 
+>>>>>>> f7bf719b0eb626c63076579bdce4f347c5d845c0
 }
 
 //Creates array of Person
